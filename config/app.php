82c62d149b33c75f247000df539fad1296a3ab7d
ahmedtawfek28 <ahmedtawfek28@gmail.com>
2019-10-13 15:48:03 +0200

    multi localization on admin

--- config/app.php
+++ config/app.php
@@ -177,7 +177,8 @@ return [
         Spatie\Permission\PermissionServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class
+        Intervention\Image\ImageServiceProvider::class,
+	    Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
 
     ],
 
@@ -233,7 +234,8 @@ return [
         'Html' => Collective\Html\HtmlFacade::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'LaravelLocalization' => Mcamara\LaravelLocalization\Facades\LaravelLocalization::class,
     ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -10,58 +10,68 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get( '/', function () {
 
-Route::get('/{locale}', function ($locale) {
-    App::setLocale($locale);
-    return view('welcome');
-});
+	return redirect( '/en' );
+} );
+Route::get( '/{locale}', function ( $locale ) {
+	App::setLocale( $locale );
 
-Route::get('/500', function () {
-    return view('page500');
-})->name('page500');
+	return view( 'welcome' );
+} );
+
+Route::get( '/500', function () {
+	return view( 'page500' );
+} )->name( 'page500' );
 
 /**
  * Admin login route
  */
-Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.post');
-Route::post('/admin/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+Route::get( '/admin/login', 'Auth\AdminLoginController@showLoginForm' )->name( 'admin.login' );
+Route::post( '/admin/login', 'Auth\AdminLoginController@login' )->name( 'admin.login.post' );
+Route::post( '/admin/logout', 'Auth\AdminLoginController@logout' )->name( 'admin.logout' );
 
 
 /**
  * route only for Admin profile
  */
-Route::group(['as' => 'Admin.', 'prefix' => 'Admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function () {
+Route::group( [ 'prefix' => LaravelLocalization::setLocale() ], function () {
+	Route::group( [
+		'as'         => 'Admin.',
+		'prefix'     => 'Admin',
+		'namespace'  => 'Admin',
+		'middleware' => [ 'auth', 'admin' ]
+	], function () {
+		Route::get( '/dashboard', 'HomeController@index' )->name( 'home' );
+		Route::resource( 'roles', 'RoleController' );
+		Route::resource( 'users', 'UserController' );
+		Route::resource( 'products', 'ProductController' );
+		Route::resource( 'permissioncategories', 'PermissioncategoryController' );
+		Route::resource( 'permissions', 'PermissionController' );
+		Route::resource( 'options', 'OptionController' );
+		Route::resource( 'localization', 'LocalizationController' );
+		Route::get( 'web_setting', 'WebSettingController@index' )->name( 'websetting.index' );
+		Route::put( 'web_setting', 'WebSettingController@update' )->name( 'web_setting.update' );
+		Route::resource( 'category', 'CategoryController' );
+		Route::resource( 'subcategory', 'SubCategoryController' );
 
-    Route::get('/dashboard', 'HomeController@index')->name('home');
-    Route::resource('roles','RoleController');
-    Route::resource('users','UserController');
-    Route::resource('products','ProductController');
-    Route::resource('permissioncategories','PermissioncategoryController');
-    Route::resource('permissions','PermissionController');
-    Route::resource('options','OptionController');
-    Route::resource('localization','LocalizationController');
-    Route::get('web_setting','WebSettingController@index')->name('websetting.index');
-    Route::put('web_setting','WebSettingController@update')->name('web_setting.update');
-    Route::resource('category','CategoryController');
-    Route::resource('subcategory','SubCategoryController');
-
-});
+	} );
+} );
 
 
 /**
  * customer login route
  */
-Route::get('/customer/login', 'Auth\CustomerLoginController@showLoginForm')->name('customer.login');
-Route::post('/customer/login', 'Auth\CustomerLoginController@login')->name('customer.login.post');
-Route::post('/customer/logout', 'Auth\CustomerLoginController@logout')->name('customer.logout');
+Route::get( '/customer/login', 'Auth\CustomerLoginController@showLoginForm' )->name( 'customer.login' );
+Route::post( '/customer/login', 'Auth\CustomerLoginController@login' )->name( 'customer.login.post' );
+Route::post( '/customer/logout', 'Auth\CustomerLoginController@logout' )->name( 'customer.logout' );
 
 
 /**
  * route only for customer profile
  */
-Route::group(['middleware'=>'customer'], function() {
+Route::group( [ 'middleware' => 'customer' ], function () {
 
-    Route::get('/customer/home', 'Customer\HomeController@index');
+	Route::get( '/customer/home', 'Customer\HomeController@index' );
 
-});
+} );
